d83b2fee4211380cb297cfbeb512db53f6f55c9a
HiranmayaGundu <hiranmayagundu@gmail.com>
2020-04-18 15:43:53 +0530

    Disable the word wrap in the pre component and let my code block bre… (#9)
    * Disable the word wrap in the pre componenet and let my code block breathe a little on larger screens
    
    * used theme and cleaned up about page
    
    * fix the font size

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -2,11 +2,19 @@ import React from "react";
 import Highlight, { defaultProps, Language } from "prism-react-renderer";
 import theme from "prism-react-renderer/themes/nightOwl";
 import Pre from "./Pre";
+import styled from "styled-components";
 
 interface CodeBlockProps {
   children: React.ReactNode;
   className?: string;
 }
+
+const ExpanderDiv = styled.div`
+  @media (min-width: 1024px) {
+    margin-left: -80px;
+    margin-right: -80px;
+  }
+`;
 
 const CodeBlock: React.FC<CodeBlockProps> = ({ children, className }) => {
   const language = className?.replace(/language-/, "");
@@ -24,48 +32,31 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ children, className }) => {
         getLineProps,
         getTokenProps
       }): React.ReactNode => (
-        <Pre className={className} style={{ ...style, padding: "20px" }}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
+        <ExpanderDiv>
+          <Pre className={className} style={{ ...style, padding: "20px" }}>
+            <code>
+              {tokens.map((line, i) => (
+                <div key={i} {...getLineProps({ line, key: i })}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
               ))}
-            </div>
-          ))}
-        </Pre>
+            </code>
+          </Pre>
+        </ExpanderDiv>
       )}
     </Highlight>
   );
 };
 
-const InlineCode: React.FC<CodeBlockProps> = ({ children, className }) => {
-  const language = className?.replace(/language-/, "");
-  return (
-    <Highlight
-      {...defaultProps}
-      code={children as string}
-      language={language as Language}
-      theme={theme}
-    >
-      {({
-        className,
-        style,
-        tokens,
-        getLineProps,
-        getTokenProps
-      }): React.ReactNode => (
-        <code className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </code>
-      )}
-    </Highlight>
-  );
-};
+const InlineCode = styled.code`
+  ${(props): string => `
+    background-color: ${props.theme.colors.greys[0]};
+    color: ${props.theme.colors.black};
+    font-family: ${props.theme.fonts.monospace.join()};
+    font-size: ${props.theme.fontSizes[1]}px;
+  `}
+`;
 
 export { CodeBlock, InlineCode };
--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,7 +2,7 @@ import { Box, BoxProps } from "rebass/styled-components";
 import styled from "styled-components";
 
 const Layout: React.FC<BoxProps> = styled(Box)`
-  max-width: 700px;
+  max-width: 800px;
   margin: 0 auto;
   padding-left: 16px;
   padding-right: 16px;

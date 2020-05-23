8c005398850f8b13ea5c6a490dd43f2ba01d0eb7
Hiranmaya Gundu <hiranmayagundu@gmail.com>
2020-05-23 19:20:58 +0530

    fix vitals reporting

--- functions/gtag.js
+++ functions/gtag.js
@@ -18,7 +18,7 @@ export const event = ({ action, category, label, value }) => {
 export const sendToGoogleAnalytics = ({ id, name, label, value }) => {
   window.gtag("event", name, {
     event_category:
-      label === "wev-vital" ? "Web Vitals" : "Next.js custom metric",
+      label === "web-vital" ? "Web Vitals" : "Next.js custom metric",
     event_label: id,
     value: Math.round(name === "CLS" ? value * 1000 : value),
     non_interaction: true,

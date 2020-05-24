53e6452a62fb879bc4066dfe5824401088b4ec2a
Hiranmaya Gundu <hiranmayagundu@gmail.com>
2020-05-24 18:30:53 +0530

    react to window if prefernce not stored

--- components/ColorModeContext.tsx
+++ components/ColorModeContext.tsx
@@ -1,7 +1,9 @@
+/* eslint-disable mdx/no-unused-expressions */
 // This is from https://joshwcomeau.com/gatsby/dark-mode/.
 // It is a fantastic blog post on how to avoid the
 // horrible flash effect when having themes in SSG.
 import * as React from "react";
+import { ChildrenOnlyProps } from "./Constants";
 
 import {
   COLORS,
@@ -22,22 +24,46 @@ export const ColorModeContext = React.createContext<ColorModeType>({
   },
 });
 
-interface ColorModeProviderProps {
-  children: React.ReactNode;
-}
+const changeColorMode = (root: HTMLElement, newValue: ThemeType): void => {
+  Object.entries(COLORS).forEach(([name, colorByTheme]) => {
+    const cssVarName = `--color-${name}`;
+    root.style.setProperty(cssVarName, colorByTheme[newValue]);
+  });
+};
 
-export const ColorModeProvider: React.FC<ColorModeProviderProps> = ({
+export const ColorModeProvider: React.FC<ChildrenOnlyProps> = ({
   children,
 }) => {
   const [colorMode, rawSetColorMode] = React.useState<ThemeType | undefined>(
     undefined
   );
+
+  const matchMediaQuery = React.useRef<MediaQueryList | undefined>();
+
+  const mqlHandler = (e: MediaQueryListEvent): void => {
+    const root = window.document.documentElement;
+    if (e.matches) {
+      changeColorMode(root, "dark");
+      rawSetColorMode("dark");
+    } else {
+      changeColorMode(root, "light");
+      rawSetColorMode("light");
+    }
+  };
+
   React.useEffect(() => {
     const root = window.document.documentElement;
 
     const initialColorValue: ThemeType = root.style.getPropertyValue(
       INITIAL_COLOR_MODE_CSS_PROP
     ) as ThemeType;
+    const persistedValue = localStorage.getItem(COLOR_MODE_KEY);
+    if (typeof persistedValue !== "string") {
+      matchMediaQuery.current = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      );
+      matchMediaQuery.current.addListener(mqlHandler);
+    }
 
     rawSetColorMode(initialColorValue);
   }, []);
@@ -46,11 +72,7 @@ export const ColorModeProvider: React.FC<ColorModeProviderProps> = ({
     const setColorMode = (newValue: ThemeType): void => {
       const root = window.document.documentElement;
       localStorage.setItem(COLOR_MODE_KEY, newValue);
-      Object.entries(COLORS).forEach(([name, colorByTheme]) => {
-        const cssVarName = `--color-${name}`;
-
-        root.style.setProperty(cssVarName, colorByTheme[newValue]);
-      });
+      changeColorMode(root, newValue);
       rawSetColorMode(newValue);
     };
     return {

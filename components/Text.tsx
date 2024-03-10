5e2fe10410e09a48b0b4f219843d3223c3cdf819
Hiranmaya Gundu <hiranmaya@sigmacomputing.com>
2024-03-10 13:36:41 -0700

    more rewrite

--- components/Text.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, TextProps } from "rebass/styled-components";
-import { CSSProp } from "styled-components";
-
-const StyledText = (props: TextProps & { css?: CSSProp }): JSX.Element => (
-  <Text fontFamily="body" as="p" color="text" {...props} />
-);
-
-export default StyledText;
